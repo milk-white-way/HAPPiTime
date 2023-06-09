64a1abfa081e042d958be4e38b5932f0426b1d27
milk-white-way <tam.thien.nguyen@ndus.edu>
2023-06-09 11:50:08 -0700

    Separate source codes into Matlab and AMReX folders (again)

--- stacking.m
+++
@@ -1,4 +0,0 @@
-function i_index = stacking(j_index, k_index, param)
-    N = param.N;
-    i_index = N*(j_index - 1) + k_index;
-end
--- unstacking_single.m
+++
@@ -1,7 +0,0 @@
-function [j_index, k_index] = unstacking_single(i_index, param)
-    k_index = rem(i_index, param.N);
-    if k_index == 0
-        k_index = param.N;
-    end
-    j_index = ( i_index - k_index )/param.N + 1;
-end
